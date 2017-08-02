654bf84528879a6ad1ca5000da2acc3111778a16
Martin Muñoz <im.mmun@gmail.com>
2017-08-02 16:02:10 -0400

    Minor cleanup

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = {
 
   setupPreprocessorRegistry(type, registry) {
     registry.add("htmlbars-ast-plugin", {
-      name: "attributes-expression",
-      plugin: require("./lib/attributes-expression-transform"),
+      name: "component-attributes",
+      plugin: require("./lib/component-attributes-transform"),
       baseDir: function() {
         return __dirname;
       }
@@ -15,18 +15,18 @@ module.exports = {
   },
 
   included() {
-    this.import('vendor/ember-component-attributes/index.js');
+    this.import("vendor/ember-component-attributes/index.js");
   },
 
   treeForVendor(rawVendorTree) {
-    let babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
+    let babelAddon = this.addons.find(addon => addon.name === "ember-cli-babel");
 
     let transpiledVendorTree = babelAddon.transpileTree(rawVendorTree, {
-      'ember-cli-babel': {
+      "ember-cli-babel": {
         compileModules: false
       }
     });
 
     return transpiledVendorTree;
-  },
+  }
 };
