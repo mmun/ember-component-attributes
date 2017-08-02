590f6d11c1e5218d0c27fbc74e24c72a9b3eb548
Martin Muñoz <im.mmun@gmail.com>
2017-08-02 16:13:23 -0400

    Fix bugs occuring in Ember 2.8

--- vendor/ember-component-attributes/index.js
+++ vendor/ember-component-attributes/index.js
@@ -15,7 +15,15 @@
         }
 
         if (this.attributeBindings) {
-          this.attributeBindings = attributeBindingsOverride.concat(this.attributeBindings);
+          let attributeBindings = this.attributeBindings.filter(microsyntax => {
+            // See https://github.com/emberjs/ember.js/blob/6a6f279df3b1a0979b5fd000bf49cd775c720f01/packages/ember-glimmer/lib/utils/bindings.js#L59-L73
+            let colonIndex = microsyntax.indexOf(":");
+            let attribute = colonIndex === -1 ? microsyntax : microsyntax.substring(colonIndex + 1);
+
+            return attributes.indexOf(attribute) === -1;
+          });
+
+          this.attributeBindings = attributeBindingsOverride.concat(attributeBindings);
         } else {
           this.attributeBindings = attributeBindingsOverride;
         }
