b8d08b990108cf42f22e2a196007d99ea34282e4
Robert Jackson <rjackson@linkedin.com>
2017-08-02 13:02:19 -0400

    Add basic sanity test for both scenarios.

--- tests/integration/components/attributes-test.js
+++ tests/integration/components/attributes-test.js
@@ -4,15 +4,21 @@ import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('attributes', 'Integration | Component | attributes', {
   integration: true,
-
-  beforeEach() {
-    this.register('component:x-foo', Component.extend());
-  }
 });
 
-test('it works!', function(assert) {
+test('it works when component has no existing attribute bindings', function(assert) {
+  this.register('component:x-foo', Component.extend());
 
-  this.render(hbs`{{x-foo (attributes data-foo="bar")}}`);
+  this.render(hbs`{{x-foo (attributes data-foo="bar")}} {{x-foo}}`);
 
   assert.equal(this._element.querySelectorAll('[data-foo="bar"]').length, 1);
+});
+
+test('it works when component has existing attribute bindings', function(assert) {
+  this.register('component:x-foo', Component.extend({ attributeBindings: ['dataDerp:data-derp'] }));
+
+  this.render(hbs`{{x-foo (attributes data-foo="bar") dataDerp="lol"}} {{x-foo dataDerp="hehe"}}`);
+
+  assert.equal(this._element.querySelectorAll('[data-foo="bar"]').length, 1);
+  assert.equal(this._element.querySelectorAll('[data-derp]').length, 2);
 });
